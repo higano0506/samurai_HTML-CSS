8bf210472ada78a292d4acc54a72b77ce800dcc6
EC2 Default User <ec2-user@ip-172-31-4-130.ap-northeast-1.compute.internal>
2020-09-20 13:04:26 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// アラートの問題---------------------------------------------------------------------------
+// function test() {
+  // 処理内容を記述
+//   alert("Hello World")
+// }
+// テキストの内容の出力---------------------------------------------------------------------
+// function keisan(){
+    // var nedan =0;
+    // var suuryou =0;
+    
+// if (document.getElementById("nedan").value != ""){
+//     nedan = parseInt(document.getElementById("nedan").value);
+// }
+    
+// if (document.getElementById("suuryou").value != ""){
+//     suuryou = parseInt(document.getElementById("suuryou").value);
+// }
+//     alert(nedan * suuryou);
+// 
+
+// 3項演算子の問題---------------------------------------------------------------------------------
+//     var nedan =  (document.getElementById("nedan").value != "") ? parseInt(document.getElementById("nedan").value) : "0" ;
+//     var suuryou = (document.getElementById("suuryou").value != "") ? parseInt(document.getElementById("nedan").value != "") : "0" ;
+    
+// alert("金額は" + nedan * suuryou);
+// }
+// consoleの出力-------------------------------------------------------------------------------------
+function test(){
+    console.log("Hello World");
+}
+
